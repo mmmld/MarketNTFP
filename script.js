59c22e495f43b74335751059897153b1515e22d3
marielou-estimeo <marielou@estimeo.com>
2023-04-24 16:28:55 +0200

    adding a js script to add and get sellers

---
+++ script.js
@@ -0,0 +1,16 @@
+/**
+ * 
+ * Gets all the sellers listed for one products in the database in the listed_sellers
+ * @param {String} product: can be honey, shea_butter, neem_oil, soap, herbal. 
+ * @returns a boolean, true if some sellers are listed for the product, false otherwise
+ */
+function getSellers(product) {
+    /**
+     *  TODO: need to actually implement but the listed_sellers object (declared in buyer.vxml) should be something like: 
+     *          if no seller for that product then empty list and return false
+     *          if there are sellers, the listed_sellers = [{region, quantity, price, callerNumber}] and return true
+     **/
+
+    listed_sellers = [];
+    return false;
+}
